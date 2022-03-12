1a1709de01dd75ab8c230e445669ca3e50d88b8e
Luis Antonio Páramo Romero <lparamoromero@gmail.com>
2022-03-11 18:24:22 -0600

    hola

---
+++ src/app/components/personalizacion/personalizacion.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+@Component({
+  selector: 'personalizacion',
+  templateUrl: './personalizacion.component.html',
+  styleUrls: ['./personalizacion.component.css']
+})
+export class PersonalizacionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
